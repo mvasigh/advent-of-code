8625c6406732df79c8db4854efb166ebae84d84f
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-12-08 00:21:53 -0500

    completed day 2

--- day-2/index.js
+++ day-2/index.js
@@ -6,10 +6,26 @@ const inputFile = path.resolve(__dirname, 'input.txt');
 
 function main() {
   const raw = fs.readFileSync(inputFile, 'utf-8');
-  const input = setInputs(parse(raw), 12, 2);
-  const output = runProgram(input);
+  const program = parse(raw);
 
-  console.log(`The value at position 0 is ${output[0]}`);
+  let noun = 2;
+  let verb = 2;
+  let output = null;
+  const TARGET = 19690720;
+
+  while (output !== TARGET) {
+    const input = setInputs(program, noun, verb);
+    output = runProgram(input)[0];
+    if (output === TARGET) break;
+    if (output > TARGET) {
+      verb++;
+      noun = 0;
+    }
+    noun++;
+  }
+
+  console.log('Done!');
+  console.log(`Output is ${output}, 100 * noun + verb is ${100 * noun + verb}`);
 }
 
 main();
