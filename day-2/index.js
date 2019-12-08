074a61d92947537476417f4932e5f980633e87c8
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-12-08 00:08:53 -0500

    refactor day 2 part 1

--- day-2/index.js
+++ day-2/index.js
@@ -1,27 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const { runProgram } = require('./lib');
+const { parse, setInputs, runProgram } = require('./lib');
 
 const inputFile = path.resolve(__dirname, 'input.txt');
 
 function main() {
-  const input = fs.readFileSync(inputFile, 'utf-8');
+  const raw = fs.readFileSync(inputFile, 'utf-8');
+  const input = setInputs(parse(raw), 12, 2);
+  const output = runProgram(input);
 
-  // Do some corrections per instructions!
-  // - replace position 1 with the value 12
-  // - replace position 2 with the value 2
-  const inputArr = input
-    .trim()
-    .split(',')
-    .map(val => parseInt(val));
-  inputArr[1] = 12;
-  inputArr[2] = 2;
-  const correctedInput = inputArr.join(',');
-
-  const output = runProgram(correctedInput);
-  const outputArr = output.split(',');
-
-  console.log(`The value at position 0 is ${outputArr[0]}`);
+  console.log(`The value at position 0 is ${output[0]}`);
 }
 
 main();
--- day-2/lib.js
+++ day-2/lib.js
@@ -1,8 +1,18 @@
-exports.runProgram = input => {
-  const program = input
+exports.stringify = arr => arr.join(',');
+
+exports.parse = str =>
+  str
     .trim()
     .split(',')
-    .map(op => parseInt(op));
+    .map(val => parseInt(val));
+
+exports.runProgram = input => {
+  if (typeof input !== 'string' && !Array.isArray(input)) {
+    throw new Error('Invalid input, must be an Array or a string');
+  }
+
+  const program = typeof input === 'string' ? this.parse(input) : [...input];
+
   for (let p = 0; p < program.length; p += 4) {
     const op = program[p];
     if (op === 1) {
@@ -15,5 +25,13 @@ exports.runProgram = input => {
       break;
     }
   }
-  return program.join(',');
+
+  return program;
+};
+
+exports.setInputs = (program, noun, verb) => {
+  const _program = [...program];
+  _program[1] = noun;
+  _program[2] = verb;
+  return _program;
 };
