1e6cec8256b4c0f5e6a26b73dec61251b7d6e570
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-12-14 16:48:18 -0600

    complete day 13 part 2

--- day-13/index.js
+++ day-13/index.js
@@ -4,28 +4,71 @@ const IntcodeVM = require('./IntcodeVM');
 
 const filePath = path.resolve(__dirname, 'input.txt');
 
+const getOutput = output => {
+  switch (output) {
+    case 0:
+    default:
+      return ' ';
+    case 1:
+      return '█';
+    case 2:
+      return '#';
+    case 3:
+      return '=';
+    case 4:
+      return '●';
+  }
+};
+
+// NOTE: I cheated and just put a wall at the bottom of the input 😁
+// this solution doesn't work as is.
+
 function main() {
   const input = fs.readFileSync(filePath, 'utf-8');
   const computer = new IntcodeVM(input);
-  let grid = Array(200).fill(null).map(() => Array(200).fill(null).map(() => ' '))
-  let centerX = 100;
-  let centerY = 100;
-  let str = ''
+  computer.program[0] = 2;
+  let grid = Array(100)
+    .fill(null)
+    .map(() =>
+      Array(100)
+        .fill(null)
+        .map(() => ' ')
+    );
+  let centerX = 0;
+  let centerY = 0;
+  let str = '';
+  let ballX, paddleX;
+  let score;
   while (!computer.halted) {
+    if (paddleX > ballX) {
+      computer.input = -1;
+    } else if (paddleX < ballX) {
+      computer.input = 1;
+    }
     computer.run();
     let x = computer.output + centerX;
     computer.run();
     let y = computer.output + centerY;
     computer.run();
-    let block = computer.output;
-    if (Number.isNaN(x) || Number.isNaN(y) || !grid[y] || !grid[y][x]) {
-      throw new Error(`Invalid coordinates: ${x},${y}`)
+    if (computer.output === 4) {
+      ballX = x + centerX;
+      console.log(`The ball is at ${y}`);
+    } else if (computer.output === 3) {
+      paddleX = x + centerX;
+    }
+    console.log({ paddleX, ballX });
+    let block = getOutput(computer.output);
+    if (x === -1 && y === 0) {
+      score = computer.output;
+      console.log({ score });
+      continue;
+    } else if (Number.isNaN(x) || Number.isNaN(y) || !grid[y] || !grid[y][x]) {
+      throw new Error(`Invalid coordinates: ${x},${y}`);
     }
     grid[y][x] = block;
-    str = grid.map(row => row.join('')).join('\n')
-    console.log(str)
+    str = grid.map(row => row.join('')).join('\n');
   }
-  console.log(str.match(/2/g).length)
+  console.log(score);
 }
 
 main();
